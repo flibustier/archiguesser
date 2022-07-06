4a9ca41ceb0e3998bf67d246c285101f47e44727
Jonathan Platteau <jplatteau.externe@m6.fr>
2022-07-06 21:01:29 +0200

    refactor(app): use less local storage

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { ref, reactive, computed } from "vue";
+import { reactive, computed } from "vue";
 import GuessingForm from "./components/guessing/GuessingForm.vue";
 import GuessingHistory from "./components/guessing/GuessingHistory.vue";
 import PictureDisplay from "./components/picture-display/PictureDisplay.vue";
@@ -13,22 +13,20 @@ const { dayNumber, answer } = reactive(
   getDayNumberAndAnswer(urlParameters.get("day"))
 );
 
-const currentRound = ref(1);
-const hasWon = ref(false);
 const guesses: string[] = reactive([]);
 const stats = JSON.parse(localStorage.getItem("stats") || "{}");
 
 if (localStorage.getItem("dayNumber") === dayNumber.toString()) {
-  currentRound.value = parseInt(localStorage.getItem("currentRound") || "1");
-  hasWon.value = localStorage.getItem("hasWon") === "true" || false;
   guesses.push(...JSON.parse(localStorage.getItem("guesses") || "[]"));
 } else {
   localStorage.setItem("dayNumber", dayNumber.toString());
-  localStorage.setItem("currentRound", "1");
-  localStorage.setItem("hasWon", "false");
   localStorage.setItem("guesses", JSON.stringify([]));
 }
 
+const currentRound = computed(() => guesses.length + 1);
+const hasWon = computed(
+  () => guesses.length > 0 && guesses[guesses.length - 1] === answer
+);
 const isGameEnded = computed(() => currentRound.value === 7 || hasWon.value);
 
 const updateStats = (score: number) => {
@@ -44,17 +42,11 @@ const onSubmittedGuess = (guess: string) => {
   guesses.push(guess);
   localStorage.setItem("guesses", JSON.stringify(guesses));
   if (guess === answer) {
-    hasWon.value = true;
-    localStorage.setItem("hasWon", "true");
-    updateStats(currentRound.value);
-    currentRound.value = 6;
-    localStorage.setItem("currentRound", currentRound.value.toString());
+    updateStats(currentRound.value - 1);
 
     return;
   }
 
-  ++currentRound.value;
-  localStorage.setItem("currentRound", currentRound.value.toString());
   if (isGameEnded.value) {
     updateStats(0);
 
@@ -67,7 +59,7 @@ const onSubmittedGuess = (guess: string) => {
   <HeaderNavigator />
   <main>
     <PictureDisplay
-      :max-pictures="Math.min(currentRound, 6)"
+      :max-pictures="isGameEnded ? 6 : Math.min(currentRound, 6)"
       :day-number="dayNumber"
     />
 
