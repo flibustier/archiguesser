b37865ff76f1377382f59de559f07157685815ea
Jonathan Platteau <jplatteau.externe@m6.fr>
2022-07-09 19:00:18 +0200

    feat(scripts): improve jpg quality

--- scripts/convert-jpg.sh
+++ scripts/convert-jpg.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env sh
 set -e
 
-magick convert -resize 800x500 -quality 90 -scene 1 ./public/$1/*.jpg ./public/$1/%d.jpg
+magick convert -resize 900x600 -quality 90 -scene 1 ./public/$1/*.jpg ./public/$1/%d.jpg
--- scripts/convert-webp.sh
+++ scripts/convert-webp.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env sh
 set -e
 
-magick convert -resize 800x500 -scene 1 ./public/$1/*.webp ./public/$1/%d.jpg
+magick convert -resize 900x600 -scene 1 ./public/$1/*.webp ./public/$1/%d.jpg
 rm ./public/$1/*.webp
