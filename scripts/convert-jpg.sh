9c65b2872c4b724e864d6f6ca05c9f6123d4304e
Jonathan Platteau <jplatteau.externe@m6.fr>
2022-10-04 10:49:28 +0200

    feat(pictures): separate source folder

--- scripts/convert-jpg.sh
+++ scripts/convert-jpg.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env sh
 set -e
 
-magick convert -resize 900x600 -quality 90 -scene 1 ./public/$1/*.jpg ./public/$1/%d.jpg
+mkdir -p ./public/$1
+magick convert -resize 900x600 -quality 90 -scene 1 ./sources/$1/*.jpg ./public/$1/%d.jpg
