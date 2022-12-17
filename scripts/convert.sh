8510d35ebd94b110179ede843bff7c0be8a9a434
Jonathan Platteau <jplatteau.externe@m6.fr>
2022-12-17 23:07:36 +0100

    feat(data): add day 169, 170, 171

--- scripts/convert.sh
+++ scripts/convert.sh
@@ -2,5 +2,5 @@
 set -e
 
 mkdir -p ./public/$1
-magick convert -resize 900x600 -scene 1 ./sources/$1/* ./public/$1/%d.jpg
+magick convert -resize 900x600 -scene 1 ./sources/$1/[1-6].* ./public/$1/%d.jpg
 magick convert -define jpeg:size=400x400 ./sources/$1/1.* -thumbnail 200x200^ -gravity center -extent 200x200 +profile "*" ./public/$1/0.jpg
