858f1ad0d5d85e7e05f3d8941afceba4c565ec4a
Jonathan Platteau <jplatteau.externe@m6.fr>
2023-02-10 23:21:53 +0100

    feat(scripts): add archdaily support to download script

--- scripts/download.js
+++ scripts/download.js
@@ -5,12 +5,12 @@ const { basename, join } = require("path");
 
 const main = async () => {
   if (process.argv.length < 4) {
-    console.log(
-      `Usage: yarn download [WIKIARQUITECTURA_URL] [DESTINATION_DIRECTORY]`
-    );
+    console.log(`Usage: yarn download [URL] [DESTINATION_DIRECTORY]`);
+
+    return;
   }
 
-  const wikiURL = process.argv[2];
+  const url = process.argv[2];
   const destinationDirectory = join("sources", process.argv[3]);
 
   if (!existsSync(destinationDirectory)) {
@@ -18,7 +18,9 @@ const main = async () => {
     mkdirSync(destinationDirectory);
   }
 
-  const imageURLs = await extractImageURLs(wikiURL);
+  const imageURLs = await extractImageURLs(url);
+
+  console.log(imageURLs);
 
   await Promise.all(imageURLs.map(downloadFile(destinationDirectory)));
 
@@ -34,7 +36,15 @@ const downloadFile = (directory) => (url) =>
     response.data.pipe(createWriteStream(`./${directory}/${basename(url)}`))
   );
 
-const imageRegex = /'(https.+)\-\d+x\d+\.jpg\??[^']*'/g;
+const uniq = (array) => [...new Set(array)];
+
+const patchWikiArquitecturaURL = (thumbnailURL) =>
+  thumbnailURL.replace(/-\d+x\d+\.jpg/, ".jpg");
+
+const patchArchDailyImageURL = (thumbnailURL) =>
+  thumbnailURL.replace(/newsletter|\w+_jpg/, "original");
+
+const imageRegex = /'(https:[\.\/\w\d_-]+\.jpg)\??[^']*'/g;
 
 const extractImageURLs = async (
   url = "https://en.wikiarquitectura.com/building/cube-houses/"
@@ -42,8 +52,10 @@ const extractImageURLs = async (
   try {
     const { data: html } = await axios.get(url);
 
-    const imageURLs = [...html.matchAll(imageRegex)].map(
-      ([, extracted]) => extracted + ".jpg"
+    const imageURLs = uniq(
+      [...html.matchAll(imageRegex)]
+        .map(([, extracted]) => patchArchDailyImageURL(extracted))
+        .map(patchWikiArquitecturaURL)
     );
 
     return imageURLs;
