247d5124af61e4aa64d4a08b9ceaad841bacd72c
Jonathan Platteau <jonathan.platteau@yahoo.fr>
2023-05-28 16:32:09 +0200

    feat(daily-selector): handle years for date

--- src/DailySelector.ts
+++ src/DailySelector.ts
@@ -2,26 +2,26 @@ import answers from "./assets/answers.json";
 import monuments from "./assets/monuments.json";
 import years from "./assets/years.json";
 
-// Fixme: add year
-const DDAY = 182;
+// Here the first day was July the 2nd of 2022
+const STARTING_DATE = Date.UTC(2022, 6, 1);
 
-// 1 to 365/366
-const getDayOfYear = (date = new Date()): number => {
+const getNumberOfDaysDifferenceWithStartingDate = (date: Date): number => {
   const timestamp1 = Date.UTC(
     date.getFullYear(),
     date.getMonth(),
     date.getDate()
   );
-  const timestamp2 = Date.UTC(date.getFullYear(), 0, 0);
-  const differenceInMilliseconds = timestamp1 - timestamp2;
+  const differenceInMilliseconds = timestamp1 - STARTING_DATE;
   const differenceInDays = differenceInMilliseconds / 1000 / 60 / 60 / 24;
 
   return differenceInDays;
 };
 
-// fixme: 2024 is a leap year, 2024-12-31 will collide with 2025-01-01
-export const getRealDayNumber = (): number =>
-  (getDayOfYear() + 365 - DDAY) % 365;
+export const getRealDayNumber = (): number => {
+  const now = new Date();
+
+  return getNumberOfDaysDifferenceWithStartingDate(now);
+};
 
 export const lastDay = () => parseInt(Object.keys(answers).pop() || "1");
 
