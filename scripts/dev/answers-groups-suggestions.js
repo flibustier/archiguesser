f2b16bd4ddc78d4901afa8f11fb1ea2b7667dbcc
Jonathan Platteau <jonathan.platteau@yahoo.fr>
2023-07-12 00:20:10 +0200

    feat(scripts): add categories to groups suggestions script

--- scripts/dev/answers-groups-suggestions.js
+++ scripts/dev/answers-groups-suggestions.js
@@ -9,7 +9,12 @@ for (const day of data) {
     .slice(1)
     .flatMap((s) => s.split(", "))
     .map((s) => s.trim())
+    .concat(day.categories)
     .forEach((s) => (stats[s] = (stats[s] || 0) + 1));
 }
 
-console.log(Object.entries(stats).sort((a, b) => b[1] - a[1]));
+console.log(
+  Object.entries(stats)
+    .sort((a, b) => b[1] - a[1])
+    .filter(([, count]) => count > 2)
+);
