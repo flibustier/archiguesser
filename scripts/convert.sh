a46bb5e5464bdc739498af00e5f8449cc5b684ef
Jonathan Platteau <jonathan.platteau@yahoo.fr>
2023-10-08 00:38:17 +0200

    feat(scripts): convert for categories

--- scripts/convert.sh
+++ scripts/convert.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env sh
 set -e
 
-mkdir -p ./public/$1
-magick convert -resize 900x600 -sharpen 0x0.5 -scene 1 ./sources/$1/[1-6].* ./public/$1/%d.jpg
-magick convert -define jpeg:size=400x400 ./sources/$1/1.* -thumbnail 200x200^ -gravity center -extent 200x200 +profile "*" ./public/$1/0.jpg
+if [[ "$1" == categories ]]
+then
+    magick convert -define jpeg:size=400x400 ./sources/$1/* -thumbnail 200x200^ -gravity center -extent 200x200 +profile "*" -set filename:base "%[basename]" "./public/categories/%[filename:base].jpg"
+else
+    mkdir -p ./public/$1
+    magick convert -resize 900x600 -sharpen 0x0.5 -scene 1 ./sources/$1/[1-6].* ./public/$1/%d.jpg
+    magick convert -define jpeg:size=400x400 ./sources/$1/1.* -thumbnail 200x200^ -gravity center -extent 200x200 +profile "*" ./public/$1/0.jpg
+fi
