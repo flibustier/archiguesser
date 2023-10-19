6dfac7ef4cbb0669204e66280e49d390407f7be9
Jonathan Platteau <jonathan.platteau@yahoo.fr>
2023-10-19 23:09:59 +0100

    feat(scripts): limit quality preset

--- scripts/convert.sh
+++ scripts/convert.sh
@@ -6,6 +6,6 @@ then
     magick convert -define jpeg:size=400x400 ./sources/$1/* -thumbnail 200x200^ -gravity center -extent 200x200 +profile "*" -set filename:base "%[basename]" "./public/categories/%[filename:base].jpg"
 else
     mkdir -p ./public/$1
-    magick convert -interlace plane -strip -resize 900x600 -sharpen 0x0.5 -scene 1 ./sources/$1/[1-6].* ./public/$1/%d.jpg
+    magick convert -interlace plane -strip -quality 95 -resize 900x600 -sharpen 0x0.5 -scene 1 ./sources/$1/[1-6].* ./public/$1/%d.jpg
     magick convert -interlace plane -strip -define jpeg:size=400x400 ./sources/$1/1.* -thumbnail 200x200^ -gravity center -extent 200x200 +profile "*" ./public/$1/0.jpg
 fi
