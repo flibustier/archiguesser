5c9bbc14043cac1b4e1562aae1b3c07b1464a469
Jonathan Platteau <jonathan.platteau@yahoo.fr>
2024-10-16 20:32:12 +0200

    feat(prettier): upgrade with new eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,11 +1,25 @@
 // eslint.config.mjs
 import pluginVue from "eslint-plugin-vue";
 import vueTsEslintConfig from "@vue/eslint-config-typescript";
+import prettierConfig from "@vue/eslint-config-prettier";
+// import prettierConfig from "@vue/eslint-config-prettier/skip-formatting";
 
 export default [
-  { ignores: ["dist"] },
   ...pluginVue.configs["flat/essential"],
   ...vueTsEslintConfig({
     extends: ["recommended", "stylistic"],
   }),
+  {
+    ignores: ["dist"],
+  },
+  {
+    rules: {
+      "@typescript-eslint/no-explicit-any": "warn",
+      "@typescript-eslint/no-unused-vars": [
+        "warn",
+        { ignoreRestSiblings: true },
+      ],
+    },
+  },
+  prettierConfig,
 ];
