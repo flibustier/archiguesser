14658593bb1cc8ca18efd13fca0650df2743be98
Jonathan Platteau <jonathan.platteau@yahoo.fr>
2024-12-15 23:18:19 +0100

    tests(e2e): update tests e2e

--- tests/challenges.spec.ts
+++ tests/challenges.spec.ts
@@ -5,6 +5,10 @@ const __dirname = dirname(fileURLToPath(import.meta.url));
 import { test, expect } from "@playwright/test";
 
 test("play test challenge", async ({ page }) => {
+  await page.addInitScript(() => {
+    window.localStorage.setItem("clientID", "test");
+  });
+
   await page.goto("/?challenge=test");
 
   await expect(page).toHaveScreenshot({
