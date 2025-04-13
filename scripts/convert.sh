8f8538574bf7204bfb763a1c2c66cfba7c42989b
Jonathan Platteau <jonathan.platteau@yahoo.fr>
2025-04-13 17:07:14 +0200

    chore: update imagemagick to v7, reconvert existing

--- scripts/convert.sh
+++ scripts/convert.sh
@@ -6,7 +6,7 @@ then
     magick convert -define jpeg:size=400x400 ./sources/$1/* -thumbnail 200x200^ -gravity center -extent 200x200 +profile "*" -set filename:base "%[basename]" "./public/categories/%[filename:base].jpg"
 else
     mkdir -p ./public/$1
-    magick convert -interlace plane -strip -quality 85 -resize 1300x600 -sharpen 0x0.5 -auto-orient -scene 1 ./sources/$1/[1-6].* ./public/$1/%d.jpg
-    magick convert -interlace plane -strip -define jpeg:size=400x400 ./sources/$1/1.* -thumbnail 200x200^ -gravity center -extent 200x200 +profile "*" ./public/$1/0.jpg
+    magick ./sources/$1/[1-6].* -interlace plane -strip -quality 85 -resize 1300x600 -sharpen 0x0.5 -auto-orient -scene 1 ./public/$1/%d.jpg
+    magick ./sources/$1/1.* -interlace plane -strip -define jpeg:size=400x400 -thumbnail 200x200^ -auto-orient -gravity center -extent 200x200 +profile "*" ./public/$1/0.jpg
     echo "http://localhost:4000/?day=$1"
 fi
