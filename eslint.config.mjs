7e231337aef87c1354c5eca355d6d4535c20e0fd
Jonathan Platteau <jonathan.platteau@yahoo.fr>
2026-03-31 22:42:30 +0200

    feat(data): add day 1370 (640)

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,16 +2,15 @@
 import pluginVue from "eslint-plugin-vue";
 import vueTsEslintConfig from "@vue/eslint-config-typescript";
 import prettierConfig from "@vue/eslint-config-prettier";
-// import prettierConfig from "@vue/eslint-config-prettier/skip-formatting";
 
 export default [
-  ...pluginVue.configs["flat/essential"],
+  {
+    ignores: ["dist/**", "scripts/*.ts"],
+  },
+  ...pluginVue.configs["flat/recommended"],
   ...vueTsEslintConfig({
     extends: ["recommended", "stylistic"],
   }),
-  {
-    ignores: ["dist", "scripts/*.ts"],
-  },
   {
     rules: {
       "@typescript-eslint/no-explicit-any": "warn",
